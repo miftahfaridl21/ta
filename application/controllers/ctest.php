31b03a7b19b3007bc85b8b956277822218a28ed9
Miftah Faridl <miftah.faridl.git@gmail.com>
2013-07-17 20:37:03 +0700

    comit pertama

---
+++ application/controllers/ctest.php
@@ -0,0 +1,17 @@
+<?php
+
+	class Ctest extends CI_Controller {
+		
+		function index() {
+		
+			echo "Testing....";
+		
+		}
+		
+		function hasil() {
+			
+			echo "Fungsi Hasil....";
+			
+		}
+		
+	}
